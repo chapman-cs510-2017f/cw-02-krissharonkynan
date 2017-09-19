1180c3da36ebc7e6c532f86d7775686017071fb6
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-19 16:58:38 -0700

    Instructor comments

--- createfiles.sh
+++ createfiles.sh
@@ -2,12 +2,12 @@
 
 mkdir tmpfiles
 
-cd tmpfiles
+cd tmpfiles          ### It is polite to return to the original directory when done
 
-touch file{001..100}.tmp
+touch file{001..100}.tmp  ### You could have touched the files in tmpfiles already
 
 for tmpfiles in *.tmp;
 do mv "$tmpfiles" "${tmpfiles//file/Temporary file }";
 done 
  
-
+### cd - ### This returns to the last directory at the end of the script
